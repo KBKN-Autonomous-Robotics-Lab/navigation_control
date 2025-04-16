b5b5978649f43723097a6e2ffa59fe50c8e1d091
kugurofu <tenten31569@icloud.com>
2025-04-16 08:46:11 +0000

    add human and stopsign files

--- navigation_control/setup.py
+++ navigation_control/setup.py
@@ -35,6 +35,8 @@ setup(
         'gps_waypoint = navigation_control.gps_waypoint:main',
         'stop_sign_detection = navigation_control.stop_sign_detection:main',
         'human_realsense = navigation_control.human_realsense:main',
+        'stop_sign = navigation_control.stop_sign:main',
+        'pedestrian = navigation_control.pedestrian:main',
         ],
     },
 )
