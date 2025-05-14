2edd064eec84c828a7433f8e5e214ce3d4e3e918
kugurofu <tenten31569@icloud.com>
2025-05-14 13:11:42 +0000

    add rviz goal point

--- navigation_control/navigation_control/gps_waypoint.py
+++ navigation_control/navigation_control/gps_waypoint.py
@@ -1,64 +1,114 @@
-# rclpy (ROS 2のpythonクライアント)の機能を使えるようにします。
 import rclpy
-# rclpy (ROS 2のpythonクライアント)の機能のうちNodeを簡単に使えるようにします。こう書いていない場合、Nodeではなくrclpy.node.Nodeと書く必要があります。
 from rclpy.node import Node
-from sensor_msgs.msg import Imu, NavSatFix
+from sensor_msgs.msg import NavSatFix
 import numpy as np
-import time
-import threading
-from geopy.distance import geodesic  
 import tkinter as tk
-import geometry_msgs.msg as geometry_msgs
-import nav_msgs.msg as nav_msgs
 import math
 from rclpy.qos import QoSProfile, QoSDurabilityPolicy, QoSHistoryPolicy, QoSReliabilityPolicy
-import std_msgs.msg as std_msgs
-import sensor_msgs.msg as sensor_msgs
-import yaml
-import os
+import geometry_msgs.msg as geometry_msgs
+import nav_msgs.msg as nav_msgs
 import queue
-from orange_gnss.get_lonlat_movingbase_quat_ttyUSB import GPSData
 from my_msgs.srv import Avglatlon
+from geometry_msgs.msg import PoseStamped
+import threading
+from rclpy.time import Time
 
-class GPSAverageNode(Node):
+
+def rotation_xyz(pointcloud, theta_x, theta_y, theta_z):
+    rad_x = math.radians(theta_x)
+    rad_y = math.radians(theta_y)
+    rad_z = math.radians(theta_z)
+    rot_x = np.array([[ 1,               0,                0],
+                      [ 0, math.cos(rad_x), -math.sin(rad_x)],
+                      [ 0, math.sin(rad_x),  math.cos(rad_x)]])
+    
+    rot_y = np.array([[ math.cos(rad_y), 0,  math.sin(rad_y)],
+                      [               0, 1,                0],
+                      [-math.sin(rad_y), 0,  math.cos(rad_y)]])
+    
+    rot_z = np.array([[ math.cos(rad_z), -math.sin(rad_z), 0],
+                      [ math.sin(rad_z),  math.cos(rad_z), 0],
+                      [               0,                0, 1]])
+    rot_matrix = rot_z.dot(rot_y.dot(rot_x))
+    #print(f"rot_matrix ={rot_matrix}")
+    #print(f"pointcloud ={pointcloud.shape}")
+    rot_pointcloud = rot_matrix.dot(pointcloud)
+    return rot_pointcloud, rot_matrix
+
+
+def quaternion_to_euler(x, y, z, w):
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll = math.atan2(t0, t1)
+
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch = math.asin(t2)
+
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw = math.atan2(t3, t4)
+
+    return roll, pitch, yaw
+
+class GPSWaypointManager(Node):
     def __init__(self):
-        super().__init__('gps_average_node')
-        #self.subscription = self.create_subscription(NavSatFix, '/fix', self.gps_callback, 10)
-        #self.movingase_sub = self.create_subscription(Imu, "movingbase/quat", self.movingbase_callback, 1)
+        super().__init__('gps_waypoint_manager')
 
         self.data = []
         self.start_time = None
         self.is_collecting = False
-        self.waypoints = queue.Queue()  
-        #self.theta = None
+        self.waypoints = queue.Queue()
         self.count = 0
+        self.theta = None
         self.declare_parameter('Position_magnification', 1.675)
         self.Position_magnification = self.get_parameter('Position_magnification').get_parameter_value().double_value
-        
-        # service
+
         self.avg_gps_service = self.create_service(Avglatlon, 'send_avg_gps', self.receive_avg_gps_callback)
-        
-        # define waypoints
+
         self.ref_points = [
-            (35.42578984, 139.3138073), # waypoint 1
-            (35.42580947, 139.3138761), # waypoint 2
-            (35.42582577, 139.3139183), # waypoint 3
-            (35.42584276, 139.3139622), # waypoint 1
-            (35.42585746, 139.3139984), # waypoint 2
-            (35.42589533, 139.3139987), # waypoint 3
-            (35.42596721, 139.3139898), # waypoint 4
-            (35.42596884, 139.3139395) # waypoint 3
-        ]        
-        self.first_point = np.array([[0.0, 0.0, 0.0],
-                                    [5.0, 0.0, 0.0],
-                                    [10.0, 0.0, 0.0]])  # 開始点など                        
-        self.last_point = np.array([[0.0, 0.0, 0.0]])   # 終了点など
-        
-        # Tkinter
+            (35.42578984, 139.3138073), # nakaniwa hajime
+            (35.42580947, 139.3138761),
+            (35.42582577, 139.3139183),
+            (35.42584276, 139.3139622),
+            (35.42585746, 139.3139984),
+            (35.42589533, 139.3139987),
+            (35.42596721, 139.3139898),
+            (35.42596884, 139.3139395) # nakaniwa owari
+        ]
+
+        self.first_point = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]])
+        self.last_point = np.array([[0.0, 0.0, 0.0]])
+
         self.root = tk.Tk()
         self.root.bind("<Key>", self.key_input_handler)
-        self.reversed_flag = False  # bが入力されたかどうかを記録
-        
+        self.reversed_flag = False
+
+        qos_profile = QoSProfile(
+            history=QoSHistoryPolicy.KEEP_LAST,
+            reliability=QoSReliabilityPolicy.RELIABLE,
+            durability=QoSDurabilityPolicy.VOLATILE,
+            depth=10
+        )
+
+        self.goal_sub = self.create_subscription(PoseStamped, '/goal_pose', self.goal_pose_callback, qos_profile)       
+        self.odom_sub = self.create_subscription(nav_msgs.Odometry, '/fusion/odom', self.get_odom, qos_profile)
+        self.waypoint_pub = self.create_publisher(geometry_msgs.PoseArray, 'current_waypoint', qos_profile)
+        self.timer = self.create_timer(0.1, self.waypoint_manager)
+
+        self.current_waypoint = 0
+        self.stop_flag = 0
+        self.position_x = 0.0
+        self.position_y = 0.0
+        self.position_z = 0.0
+        self.theta_x = 0.0
+        self.theta_y = 0.0
+        self.theta_z = 0.0
+        self.waypoints_array = None
+        self.waypoints_array = np.array([[0.0],[0.0],[0.0]])
+        self.waypoint_range_set = 3.5
+
     def key_input_handler(self, event):
         key = event.char.lower()
         if key == 'b':
@@ -68,13 +118,42 @@ class GPSAverageNode(Node):
         elif key == 'a':
             self.get_logger().info("キー入力: 'a' を受け取りました。通常順で実行します。")
             self.reversed_flag = False
+
+    def goal_pose_callback(self, msg):
+        x = msg.pose.position.x
+        y = msg.pose.position.y
+        z = msg.pose.position.z
+        qz = msg.pose.orientation.z
+        qw = msg.pose.orientation.w
+        
+        xyz = np.vstack((x,y,z))
+        waypoint_range = np.vstack((self.waypoint_range_set,0.0,0.0))
+        yaw = self.orientation_to_yaw(qz, qw) *180/math.pi
+        xyz_range , _ = rotation_xyz(waypoint_range, 0, 0, yaw)
+        next_x = xyz[0] + xyz_range[0]
+        next_y = xyz[1] + xyz_range[1]
+        next_z = xyz[2] + xyz_range[2]
+        next_xyz = np.vstack((next_x,next_y,next_z))
+        
+        
+        self.current_waypoint = 0;
+        #self.waypoints_array = np.array([[xyz[0], xyz[1], xyz[2]], [next_xyz[0], next_xyz[1], next_xyz[2]]])
+        self.waypoints_array = np.array([[xyz[0], next_xyz[0]],[xyz[1], next_xyz[1]], [xyz[2], next_xyz[2]]])
+        #self.waypoints_array = np.stack((xyz,next_xyz))
+        
+        # クォータニオン → ヨー角（Z軸の回転）に変換
+        #angle = math.atan2(2.0 * (qw * qz), 1.0 - 2.0 * (qz * qz))
+        
+        #pose_array = self.current_waypoint_msg(self.waypoints_array[:, self.current_waypoint], 'map')
+        #self.waypoint_pub.publish(pose_array)
+        
+        self.get_logger().info(f"Received goal: x={x:.3f}, y={y:.3f}, yaw={yaw:.3f} deg")    
+        self.get_logger().info(f"self.waypoints_array:{self.waypoints_array}")    
+        self.get_logger().info(f"xyz_range:{xyz_range}")    
     
-    def movingbase_callback(self, msg):
-        if self.count == 0:
-            self.theta = msg.orientation_covariance[0]
-            self.count = 1
-    
-    # copy lonlat_to_odom function 
+    def orientation_to_yaw(self, z, w):
+        yaw = np.arctan2(2.0 * (w * z), 1.0 - 2.0 * (z ** 2))
+        return yaw
     
     def conversion(self, avg_lat, avg_lon, theta):
         #ido = self.ref_points[0]
@@ -148,208 +227,83 @@ class GPSAverageNode(Node):
             points.append(point)
 
         return points
-     
-            
+
     def receive_avg_gps_callback(self, request, response):
-        avg_lat = request.avg_lat
-        avg_lon = request.avg_lon
-        theta = request.theta
-
-        self.get_logger().info(f"サービス受信: 平均緯度={avg_lat}, 経度={avg_lon}")
-
+        avg_lat, avg_lon, theta = request.avg_lat, request.avg_lon, request.theta
         if theta is None:
             self.get_logger().warn("GPSからのthetaがまだ取得されていません。")
             response.success = False
             return response
 
-        #GPSxy = GPSData.conversion(avg_lat, avg_lon, theta)
         GPSxy = self.conversion(avg_lat, avg_lon, theta)
-
         gps_np = np.array(GPSxy)
-
-        first_point = self.first_point
-        last_point = self.last_point
-
         if self.reversed_flag:
-            first_point[:, 1] *= -1
-            last_point[:, 1] *= -1
+            self.first_point[:, 1] *= -1
+            self.last_point[:, 1] *= -1
 
-        full_waypoints = np.concatenate([first_point, gps_np, last_point], axis=0)
-        self.waypoints.put(full_waypoints.T)
-
-        self.launch_waypoint_manager()
-
+        full_waypoints = np.concatenate([self.first_point, gps_np, self.last_point], axis=0)
+        self.waypoints_array = full_waypoints.T
         response.success = True
         return response
 
-            
-    def launch_waypoint_manager(self):
-        self.get_logger().info("WaypointManager を起動します。")
-        # キューからリストに変換し、WaypointManager に渡す
-        #waypoints_list = []
-        while not self.waypoints.empty():
-            #waypoints_list.append(self.waypoints.get())
-            waypoints_array = self.waypoints.get()
-
-        #waypoints_array = np.array(waypoints_list).T  # 形状を [3, N] に変換
-
-        waypoint_manager = WaypointManager(waypoints_array)
-        rclpy.spin(waypoint_manager)
-        waypoint_manager.destroy_node()
-
-    def run(self):
-        self.root.mainloop()
-
-# C++と同じく、Node型を継承します。
-class WaypointManager(Node):
-    # コンストラクタです、PcdRotationクラスのインスタンスを作成する際に呼び出されます。
-    def __init__(self, waypoints_array):
-        # 継承元のクラスを初期化します。
-        super().__init__('waypoint_manager_node')
-        
-        qos_profile = QoSProfile(
-            history=QoSHistoryPolicy.KEEP_LAST,
-            reliability=QoSReliabilityPolicy.RELIABLE,
-            durability=QoSDurabilityPolicy.VOLATILE,
-            depth = 10
-        )
-        
-        qos_profile_sub = QoSProfile(
-            reliability=QoSReliabilityPolicy.RELIABLE,
-            durability=QoSDurabilityPolicy.VOLATILE,
-            depth = 10
-        )
-        
-        # Subscriptionを作成。
-        self.subscription = self.create_subscription(nav_msgs.Odometry,'/fusion/odom', self.get_odom, qos_profile_sub)
-        self.subscription  # 警告を回避するために設置されているだけです。削除しても挙動はかわりません。
-        
-        # タイマーを0.1秒（100ミリ秒）ごとに呼び出す
-        self.timer = self.create_timer(0.1, self.waypoint_manager)
-        
-        # Publisherを作成
-        self.current_waypoint_publisher = self.create_publisher(geometry_msgs.PoseArray, 'current_waypoint', qos_profile) #set publish pcd topic name
-        
-        #パラメータ
-        #waypoint init
-        self.current_waypoint = 0
-        self.stop_flag = 0
-        
-        #positon init
-        self.position_x = 0.0 #[m]
-        self.position_y = 0.0 #[m]
-        self.position_z = 0.0 #[m]
-        self.theta_x = 0.0 #[deg]
-        self.theta_y = 0.0 #[deg]
-        self.theta_z = 0.0 #[deg]
-        
-        self.waypoints = waypoints_array
-        print(f"self.waypoints ={self.waypoints}")
-        
-    def waypoint_manager(self):
-        self.time_stamp = self.get_clock().now().to_msg()
-        #self.get_logger().info('waypoint manager cntl')
-        position_x=self.position_x; position_y=self.position_y; 
-        theta_x=self.theta_x; theta_y=self.theta_y; theta_z=self.theta_z;
-        
-        #waypoint theta & dist
-        relative_point_x = self.waypoints[0,self.current_waypoint] - position_x
-        relative_point_y = self.waypoints[1,self.current_waypoint] - position_y
-        relative_point = np.vstack((relative_point_x, relative_point_y, self.waypoints[2,self.current_waypoint]))
-        relative_point_rot, t_point_rot_matrix = rotation_xyz(relative_point, theta_x, theta_y, -theta_z)
-        waypoint_rad = math.atan2(relative_point_rot[1], relative_point_rot[0])
-        waypoint_dist = math.sqrt(relative_point_x**2 + relative_point_y**2)
-        waypoint_theta = abs(waypoint_rad * (180 / math.pi))
-        
-        #set judge dist
-        if abs(waypoint_theta) > 90:
-            determine_dist = 8
-        else:
-            determine_dist = 3
-        #check if the waypoint reached
-        if waypoint_dist < determine_dist:
-            self.current_waypoint += 1
-            if self.current_waypoint > (len(self.waypoints[0,:]) - 1):
-                self.stop_flag = 1
-                self.get_logger().info('GOAL : stop_flag = %f' % (stop_flag))
-        self.get_logger().info('current_waypoint:x = %f, y = %f : waypoint_no = %f' % (self.waypoints[0,self.current_waypoint], self.waypoints[1,self.current_waypoint], self.current_waypoint))
-        
-        #publish
-        pose_array = self.current_waypoint_msg(self.waypoints[:,self.current_waypoint], 'map')
-        self.current_waypoint_publisher.publish(pose_array)
-        
     def get_odom(self, msg):
         self.position_x = msg.pose.pose.position.x
         self.position_y = msg.pose.pose.position.y
         self.position_z = msg.pose.pose.position.z
-        
-        flio_q_x = msg.pose.pose.orientation.x
-        flio_q_y = msg.pose.pose.orientation.y
-        flio_q_z = msg.pose.pose.orientation.z
-        flio_q_w = msg.pose.pose.orientation.w
-        
-        roll, pitch, yaw = quaternion_to_euler(flio_q_x, flio_q_y, flio_q_z, flio_q_w)
-        
-        self.theta_x = 0 #roll /math.pi*180
-        self.theta_y = 0 #pitch /math.pi*180
-        self.theta_z = yaw /math.pi*180
-        
+        x, y, z, w = msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w
+        roll, pitch, yaw = quaternion_to_euler(x, y, z, w)
+        self.theta_x, self.theta_y, self.theta_z = 0, 0, yaw * 180 / math.pi
+
+    def waypoint_manager(self):
+        #self.get_logger().info(f"test: {self.current_waypoint}")
+        #if self.waypoints_array is None or self.stop_flag:
+        #    return
+            
+        position_x, position_y = self.position_x, self.position_y
+        relative_x = self.waypoints_array[0, self.current_waypoint] - position_x
+        relative_y = self.waypoints_array[1, self.current_waypoint] - position_y
+        relative_point = np.vstack((relative_x, relative_y, self.waypoints_array[2, self.current_waypoint]))
+        rotated_point, _ = rotation_xyz(relative_point, self.theta_x, self.theta_y, -self.theta_z)
+        waypoint_rad = math.atan2(rotated_point[1], rotated_point[0])
+        waypoint_dist = math.hypot(relative_x, relative_y)
+        waypoint_theta = abs(waypoint_rad * 180 / math.pi)
+
+        determine_dist = 4.5 if abs(waypoint_theta) > 90 else 4.5
+
+        #check if the waypoint reached
+        if waypoint_dist < determine_dist:
+            #self.current_waypoint += 1
+            if self.current_waypoint < len(self.waypoints_array[0,:])-1:
+                self.current_waypoint += 1
+            else:
+                # goal:stopをtrueにしてアクションを再送信
+                a=1;
+                #self.stop_flag = True
+                #self.get_logger().info("Stop flag reset to True")
+                #self.send_action_request()
+
+        pose_array = self.current_waypoint_msg(self.waypoints_array[:, self.current_waypoint], 'map')
+        self.waypoint_pub.publish(pose_array)
+
     def current_waypoint_msg(self, waypoint, set_frame_id):
         pose_array = geometry_msgs.PoseArray()
         pose_array.header.frame_id = set_frame_id
-        pose_array.header.stamp = self.time_stamp
+        pose_array.header.stamp = self.get_clock().now().to_msg()
         pose = geometry_msgs.Pose()
         pose.position.x = float(waypoint[0])
         pose.position.y = float(waypoint[1])
         pose.position.z = float(waypoint[2])
-        
-        pose.orientation.x = 0.0
-        pose.orientation.y = 0.0
-        pose.orientation.z = 0.0
         pose.orientation.w = 1.0
         pose_array.poses.append(pose)
-        
         return pose_array
 
-def rotation_xyz(pointcloud, theta_x, theta_y, theta_z):
-    theta_x = math.radians(theta_x)
-    theta_y = math.radians(theta_y)
-    theta_z = math.radians(theta_z)
-    rot_x = np.array([[ 1,                 0,                  0],
-                      [ 0, math.cos(theta_x), -math.sin(theta_x)],
-                      [ 0, math.sin(theta_x),  math.cos(theta_x)]])
-    
-    rot_y = np.array([[ math.cos(theta_y), 0,  math.sin(theta_y)],
-                      [                 0, 1,                  0],
-                      [-math.sin(theta_y), 0, math.cos(theta_y)]])
-    
-    rot_z = np.array([[ math.cos(theta_z), -math.sin(theta_z), 0],
-                      [ math.sin(theta_z),  math.cos(theta_z), 0],
-                      [                 0,                  0, 1]])
-    rot_matrix = rot_z.dot(rot_y.dot(rot_x))
-    #print(f"rot_matrix ={rot_matrix}")
-    #print(f"pointcloud ={pointcloud.shape}")
-    rot_pointcloud = rot_matrix.dot(pointcloud)
-    return rot_pointcloud, rot_matrix
-    
-def quaternion_to_euler(x, y, z, w):
-    # クォータニオンから回転行列を計算
-    rot_matrix = np.array([
-        [1 - 2 * (y**2 + z**2), 2 * (x*y - z*w), 2 * (x*z + y*w)],
-        [2 * (x*y + z*w), 1 - 2 * (x**2 + z**2), 2 * (y*z - x*w)],
-        [2 * (x*z - y*w), 2 * (y*z + x*w), 1 - 2 * (x**2 + y**2)]
-    ])
+    def run(self):
+        self.root.mainloop()
 
-    # 回転行列からオイラー角を抽出
-    roll = np.arctan2(rot_matrix[2, 1], rot_matrix[2, 2])
-    pitch = np.arctan2(-rot_matrix[2, 0], np.sqrt(rot_matrix[2, 1]**2 + rot_matrix[2, 2]**2))
-    yaw = np.arctan2(rot_matrix[1, 0], rot_matrix[0, 0])
-    return roll, pitch, yaw
-        
 
 def main(args=None):
     rclpy.init(args=args)
-    node = GPSAverageNode()
+    node = GPSWaypointManager()
     rclpy_thread = threading.Thread(target=rclpy.spin, args=(node,))
     rclpy_thread.start()
     node.run()  
